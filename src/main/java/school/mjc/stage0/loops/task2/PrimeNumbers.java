a9f89078677fcf436dc247bfc37a8b11a2d5c715
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-24 07:33:12 +0500

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,27 @@
 package school.mjc.stage0.loops.task2;
 
+
+
 public class PrimeNumbers {
-    public void printPrimeNumbers(int printToInclusive) {
+
+
+    void printPrimeNumbers(int printToInclusive) {
+
+        int temp=2;
+
+        while (temp!=printToInclusive+1){
+            if(isPrime(temp))
+                System.out.println(temp);
+
+            temp++;
+        }
+    }
+
+    private static boolean isPrime(int n){
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
     }
 }
