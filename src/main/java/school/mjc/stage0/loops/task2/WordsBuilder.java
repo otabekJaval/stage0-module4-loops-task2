2c41851d8f728d27e561f9b1cea88b7c6e137119
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-24 07:33:24 +0500

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,12 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
-    public void buildPhrase(char... chars) {
+    public  void buildPhrase(char... chars) {
+
+
+        String s=String.valueOf(chars);
+        System.out.print(s);
+
 
     }
 }
