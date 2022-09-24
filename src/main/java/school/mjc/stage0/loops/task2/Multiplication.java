33a92f09cab65d8085f98a0954d5ca8f13ab956c
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-24 07:32:38 +0500

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int copy = 0;
+
+        if(multiplyByAndToInclusive==0)
+            return;
+
+        while (copy != Math.abs(multiplyByAndToInclusive)+1) {
+
+            System.out.println(copy*multiplyByAndToInclusive);
+
+
+            copy++;
+        }
     }
 }
