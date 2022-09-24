cedd4358271724e738743d8bd4788c4369f3afa6
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-24 07:32:49 +0500

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+
+        int temp=0;
+        while (temp!=lastPrinted+1){
+            System.out.println(temp);
+            temp++;
+        }
     }
 }
